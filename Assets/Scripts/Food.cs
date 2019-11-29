3b239f31b06eadd22f2c8cc4f25a836482b17a3f
Chantelle <chansaliba99@gmail.com>
2019-11-29 22:02:18 +0100

    Spawning Food

---
+++ Assets/Scripts/Food.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Food
+{
+    private Vector2Int foodPosition;
+    private GameObject food;
+    private int h;
+    private int w;
+
+
+    public Food(int width, int height)
+    {
+        this.w = width;
+        this.h = height;
+
+        SpawnFood();
+    }
+
+    private void SpawnFood()
+    {
+        foodPosition = new Vector2Int(Random.Range(0, w), Random.Range(0, h));
+
+        food = new GameObject("food", typeof(SpriteRenderer));
+        food.GetComponent<SpriteRenderer>().sprite = GameManger.i.foodSprite;
+        food.transform.position = new Vector2(foodPosition.x, foodPosition.y);
+    }
+  
+}
+
---
+++ Assets/Scripts/GameManger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManger : MonoBehaviour
+{
+    [SerializeField] private Snake snake;
+    [SerializeField] private Food fd;
+
+    public static GameManger i;
+
+    public Sprite snakeHeadSprite;
+    public Sprite foodSprite;
+
+    public void Start()
+    {
+        fd = new Food(5, 5);
+
+    }
+    public void Awake()
+    {
+        i = this;
+    }
+
+}
