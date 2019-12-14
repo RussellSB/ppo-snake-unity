c01bb6c224dde2322117afe1c0a435e20a7f99ec
RussellSB <r.sammutbonnici@gmail.com>
2019-12-14 05:13:56 +0100

    Added smooth dragon

--- Dragon/Assets/Scripts/Snake.cs
+++ Dragon/Assets/Scripts/Snake.cs
@@ -12,11 +12,14 @@ public class Snake : MonoBehaviour
     public GameObject tailPrefab;
 
     List<Vector2Int> tail;
+    List<int> tailRotation; //Used in congruence to tail (0 - up, 1 - down, 2 - right, 3 - left)
     List<Vector2Int> snakesize;
     
     private int snakebodysize = 0;
     bool eat;
     bool dead;
+    int headRotationCode = 1; // (0 - up, 1 - down, 2 - right, 3 - left)
+    int headRotationCode_PREV; // (0 - up, 1 - down, 2 - right, 3 - left)
 
     private float Timer;
     private float MaxTimer;
@@ -29,6 +32,7 @@ public class Snake : MonoBehaviour
         Timer = MaxTimer;
 
         tail = new List<Vector2Int>();
+        tailRotation = new List<int>();
         snakebodysize = 0;
 
         snakesize = GetFullSnake();
@@ -87,7 +91,7 @@ public class Snake : MonoBehaviour
 
     private void SpriteRotation(int r)
     {
-        if(r == 0)
+        if (r == 0)
         {
             if(gridDirection.x == 1)
             {
@@ -132,6 +136,7 @@ public class Snake : MonoBehaviour
     private void RotateLeft()
     {
         transform.Rotate(Vector3.forward * -90);
+
     }
 
     private void RotateRight()
@@ -148,6 +153,18 @@ public class Snake : MonoBehaviour
             Timer = Timer - MaxTimer;
             tail.Insert(0, gridPosition);
 
+            headRotationCode_PREV = headRotationCode;
+            headRotationCode = calcCurrHeadRotationCode();
+
+            if(headRotationCode != headRotationCode_PREV)
+            {
+                tailRotation.Insert(0, calcTurnTypeCode());
+            }
+            else
+            {
+                tailRotation.Insert(0, headRotationCode);
+            }
+
             gridPosition = gridPosition + gridDirection;
 
             if (eat)
@@ -159,19 +176,108 @@ public class Snake : MonoBehaviour
             if (tail.Count >= snakebodysize + 1)
             {
                 tail.RemoveAt(tail.Count - 1);
+                tailRotation.RemoveAt(tailRotation.Count - 1);
             }
 
             for(int i = 0; i < tail.Count; i ++)
             {
                 Vector2Int snakePosition = tail[i];
+                int rotation = tailRotation[i];
                 Vector3 p = new Vector3(snakePosition.x, snakePosition.y);
 
                 GameObject g = (GameObject)Instantiate(tailPrefab, p , Quaternion.identity);
+                GameObject childStraight = g.transform.Find("Straight").gameObject;
+                GameObject childCorner1 = g.transform.Find("Corner1").gameObject;
+                GameObject childCorner2 = g.transform.Find("Corner2").gameObject;
+                GameObject childCorner3 = g.transform.Find("Corner3").gameObject;
+                GameObject childCorner4 = g.transform.Find("Corner4").gameObject;
+
+                switch (rotation)
+                {
+                    case 0:
+                        Debug.Log("Up");
+                        break;
+                    case 1:
+                        Debug.Log("Down");
+                        break;
+                    case 2:
+                        Debug.Log("Right");
+                        g.GetComponent<Transform>().Rotate(Vector3.forward * 90);
+                        break;
+                    case 3:
+                        Debug.Log("Left");
+                        g.GetComponent<Transform>().Rotate(Vector3.forward * -90);
+                        break;
+                    case 4:
+                        Debug.Log("Upright");
+                        childStraight.SetActive(false);
+                        childCorner3.SetActive(true);
+                        break;
+                    case 5:
+                        Debug.Log("Upleft");
+                        childStraight.SetActive(false);
+                        childCorner4.SetActive(true);
+                        break;
+                    case 6:
+                        Debug.Log("Downright");
+                        childStraight.SetActive(false);
+                        childCorner1.SetActive(true);
+                        break;
+                    case 7:
+                        Debug.Log("Downleft");
+                        childStraight.SetActive(false);
+                        childCorner2.SetActive(true);
+                        break;
+                    case 8:
+                        Debug.Log("Rightup");
+                        childStraight.SetActive(false);
+                        childCorner2.SetActive(true);
+                        break;
+                    case 9:
+                        Debug.Log("Rightdown");
+                        childStraight.SetActive(false);
+                        childCorner4.SetActive(true);
+                        break;
+                    case 10:
+                        Debug.Log("Leftup");
+                        childStraight.SetActive(false);
+                        childCorner1.SetActive(true);
+                        //
+                        break;
+                    case 11:
+                        Debug.Log("Leftdown");
+                        childStraight.SetActive(false);
+                        childCorner3.SetActive(true);
+                        break;
+                }
+
                 Object.Destroy(g, MaxTimer);
             }
 
             transform.position = new Vector3(gridPosition.x, gridPosition.y);
         }
+    }
+
+    int calcCurrHeadRotationCode()
+    {
+        if (gridDirection.x == 0 && gridDirection.y == 1) return 0;
+        if (gridDirection.x == 0 && gridDirection.y == -1) return 1;
+        if (gridDirection.x == 1 && gridDirection.y == 0) return 2;
+        if (gridDirection.x == -1 && gridDirection.y == 0) return 3;
+        return 1;
+    }
+
+    int calcTurnTypeCode()
+    {
+        if (headRotationCode_PREV == 0 && headRotationCode == 2) return 4;
+        if (headRotationCode_PREV == 0 && headRotationCode == 3) return 5;
+        if (headRotationCode_PREV == 1 && headRotationCode == 2) return 6;
+        if (headRotationCode_PREV == 1 && headRotationCode == 3) return 7;
+        if (headRotationCode_PREV == 2 && headRotationCode == 0) return 8;
+        if (headRotationCode_PREV == 2 && headRotationCode == 1) return 9;
+        if (headRotationCode_PREV == 3 && headRotationCode == 0) return 10;
+        if (headRotationCode_PREV == 3 && headRotationCode == 1) return 11;
+        return 0;
     }
 
     void OnTriggerEnter2D(Collider2D collision)
--- Dragon/Assets/Scripts/SnakeAIController.cs
+++ Dragon/Assets/Scripts/SnakeAIController.cs
@@ -12,6 +12,7 @@ public class SnakeAIController : MonoBehaviour
     public GameObject tailPrefab;
 
     List<Vector2Int> tail;
+    List<int> tailRotation; //Used in congruence to tail (0 - down, 1 - left, 2 - right, 3 - up)
     List<Vector2Int> snakesize;
     
     private int snakebodysize = 0;
@@ -26,6 +27,9 @@ public class SnakeAIController : MonoBehaviour
     public bool isLeft = false;
     public bool isRight = false;
 
+    int headRotationCode = 1; // (0 - up, 1 - down, 2 - right, 3 - left)
+    int headRotationCode_PREV; // (0 - up, 1 - down, 2 - right, 3 - left)
+
     private void Awake()
     {
 
@@ -35,6 +39,7 @@ public class SnakeAIController : MonoBehaviour
         Timer = MaxTimer;
 
         tail = new List<Vector2Int>();
+        tailRotation = new List<int>();
         snakebodysize = 0;
 
         snakesize = GetFullSnake();
@@ -53,11 +58,6 @@ public class SnakeAIController : MonoBehaviour
 
     private void UserInput()
     {
-        
-        //isUp = animator.GetBool("isUp");
-        //isDown = animator.GetBool("isDown");
-        //isRight = animator.GetBool("isRight");
-        //isLeft = animator.GetBool("isLeft");
         
         if (isUp)
         {
@@ -160,6 +160,18 @@ public class SnakeAIController : MonoBehaviour
             Timer = Timer - MaxTimer;
             tail.Insert(0, gridPosition);
 
+            headRotationCode_PREV = headRotationCode;
+            headRotationCode = calcCurrHeadRotationCode();
+
+            if (headRotationCode != headRotationCode_PREV)
+            {
+                tailRotation.Insert(0, calcTurnTypeCode());
+            }
+            else
+            {
+                tailRotation.Insert(0, headRotationCode);
+            }
+
             gridPosition = gridPosition + gridDirection;
 
             if (eat)
@@ -171,20 +183,109 @@ public class SnakeAIController : MonoBehaviour
             if (tail.Count >= snakebodysize + 1)
             {
                 tail.RemoveAt(tail.Count - 1);
+                tailRotation.RemoveAt(tailRotation.Count - 1);
             }
 
-            for(int i = 0; i < tail.Count; i ++)
+            for (int i = 0; i < tail.Count; i++)
             {
                 Vector2Int snakePosition = tail[i];
+                int rotation = tailRotation[i];
                 Vector3 p = new Vector3(snakePosition.x, snakePosition.y);
 
                 GameObject g = (GameObject)Instantiate(tailPrefab, p , Quaternion.identity);
+                GameObject childStraight = g.transform.Find("Straight").gameObject;
+                GameObject childCorner1 = g.transform.Find("Corner1").gameObject;
+                GameObject childCorner2 = g.transform.Find("Corner2").gameObject;
+                GameObject childCorner3 = g.transform.Find("Corner3").gameObject;
+                GameObject childCorner4 = g.transform.Find("Corner4").gameObject;
+
+                switch (rotation)
+                {
+                    case 0:
+                        //Debug.Log("Up");
+                        break;
+                    case 1:
+                        //Debug.Log("Down");
+                        break;
+                    case 2:
+                        //Debug.Log("Right");
+                        g.GetComponent<Transform>().Rotate(Vector3.forward * 90);
+                        break;
+                    case 3:
+                        //Debug.Log("Left");
+                        g.GetComponent<Transform>().Rotate(Vector3.forward * -90);
+                        break;
+                    case 4:
+                        //Debug.Log("Upright");
+                        childStraight.SetActive(false);
+                        childCorner3.SetActive(true);
+                        break;
+                    case 5:
+                        //Debug.Log("Upleft");
+                        childStraight.SetActive(false);
+                        childCorner4.SetActive(true);
+                        break;
+                    case 6:
+                        //Debug.Log("Downright");
+                        childStraight.SetActive(false);
+                        childCorner1.SetActive(true);
+                        break;
+                    case 7:
+                        //Debug.Log("Downleft");
+                        childStraight.SetActive(false);
+                        childCorner2.SetActive(true);
+                        break;
+                    case 8:
+                        //Debug.Log("Rightup");
+                        childStraight.SetActive(false);
+                        childCorner2.SetActive(true);
+                        break;
+                    case 9:
+                        //Debug.Log("Rightdown");
+                        childStraight.SetActive(false);
+                        childCorner4.SetActive(true);
+                        break;
+                    case 10:
+                        //Debug.Log("Leftup");
+                        childStraight.SetActive(false);
+                        childCorner1.SetActive(true);
+                        //
+                        break;
+                    case 11:
+                        //Debug.Log("Leftdown");
+                        childStraight.SetActive(false);
+                        childCorner3.SetActive(true);
+                        break;
+                }
+
                 Object.Destroy(g, MaxTimer);
             }
 
             transform.position = new Vector3(gridPosition.x, gridPosition.y);
             AstarPath.active.Scan(); //Update scan
         }
+    }
+
+    int calcCurrHeadRotationCode()
+    {
+        if (gridDirection.x == 0 && gridDirection.y == 1) return 0;
+        if (gridDirection.x == 0 && gridDirection.y == -1) return 1;
+        if (gridDirection.x == 1 && gridDirection.y == 0) return 2;
+        if (gridDirection.x == -1 && gridDirection.y == 0) return 3;
+        return 1;
+    }
+
+    int calcTurnTypeCode()
+    {
+        if (headRotationCode_PREV == 0 && headRotationCode == 2) return 4;
+        if (headRotationCode_PREV == 0 && headRotationCode == 3) return 5;
+        if (headRotationCode_PREV == 1 && headRotationCode == 2) return 6;
+        if (headRotationCode_PREV == 1 && headRotationCode == 3) return 7;
+        if (headRotationCode_PREV == 2 && headRotationCode == 0) return 8;
+        if (headRotationCode_PREV == 2 && headRotationCode == 1) return 9;
+        if (headRotationCode_PREV == 3 && headRotationCode == 0) return 10;
+        if (headRotationCode_PREV == 3 && headRotationCode == 1) return 11;
+        return 0;
     }
 
     void OnTriggerEnter2D(Collider2D collision)
